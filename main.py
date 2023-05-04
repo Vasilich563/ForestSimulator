a8bffd6638754890c71ad28df16f6a805b6e9376
Vasilich563 <yackub563@mail.ru>
2023-05-04 10:04:08 +0300

    add TODO for additional task

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 #Author Vodohleb04
 
-import console_mode
 import sys
 from console_mode import play_console
 from game_main_window_gui import play_graphic_mode
+
 
 def main(*args):
     print(args)
     if not len(args) == 2:
         raise ValueError(f"Unknown arguments: {args}")
     if args[1] == "--concole" or args[1] == "-c":
-        res = console_mode.play_console()
+        res = play_console()
         print(f"Game ended with exit code {res}")
         input()
     elif args[1] == "--graphic" or args[1] == "-g":
@@ -21,5 +21,4 @@ def main(*args):
 
 if __name__ == "__main__":
     main(*sys.argv)
-
-
+    # TODO Tests and documentation
